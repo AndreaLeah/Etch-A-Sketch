b6a6d6b1d009507f2da15efc2d8a6301ad97ffeb
Angelia <angelia@Angelias-Mac-Studio-5.local>
2022-08-26 00:40:41 -0400

    Complete Basic Setup Functionality

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Global Variable Declarations
+let divvies = document.querySelectorAll('div');
+const clearBtn = document.querySelector('.reset')
+
+
 function divsWrapper (gridSize) {
     // Variable Declarations
     const container = document.querySelector('.container')
@@ -9,6 +14,15 @@ function divsWrapper (gridSize) {
         let divs = document.createElement('div');
         divs.classList.add('divs');
         container.insertAdjacentElement('beforeend', divs);
+        divs.addEventListener('mouseover', function() {
+            divs.style.backgroundColor = 'black';
+        });
     }};
 
     divsWrapper(16);
+
+    clearBtn.addEventListener('click', function () {
+        location.reload();
+    });
+
+
