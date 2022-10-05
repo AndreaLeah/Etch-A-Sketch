6bde6cf0f724cf5f7e94de3cd80042571bac0976
AndreaLeah <angelia@Angeliatudio269.lan>
2022-10-05 07:56:53 -0400

    UI and functionality updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Etch-A-Sketch
+
+
+This project is part of [The Odin Project's](https://www.theodinproject.com/lessons/foundations-etch-a-sketch) curriculum. 
+This website has been made with HTML, CSS and Vanilla JS. 
+
+<br />
+
+**How To Use:**
+
+Drag your mouse across the Etch A Sketch's screen in order to draw. When you are done, select the eraser to clean the Etch A Sketch.  
+
+Select a color from the color picker to change pen color, or select the rainbow to change to rainbow drawing.  
+
+<br />
+
+[Live Demo](https://andrealeah.github.io/Etch-A-Sketch/)
+
+**Attribution:**
+Rainbow Icon created by Freepik, [source here] (https://www.flaticon.com/free-icons/rainbow)  
+Eraser Icon created by Freepik, [source here] (https://www.flaticon.com/free-icons/eraser")  
+GitHub Icon created by Pixel Perfect - Flaticon, [source here] (https://www.flaticon.com/free-icons/github)  
+
--- main.js
+++ main.js
@@ -1,31 +1,93 @@
 // Global Variable Declarations
-let divvies = document.querySelectorAll('div');
+const container = document.querySelector('.container')
 const clearBtn = document.querySelector('.reset');
 const viewWindow = document.querySelector('.view-window');
+let gridSizeSelection = 16;
+let array = [];
+// color picker element & color from picker
+const colorPicker = document.querySelector("#color-picker");
+let chosenColor = colorPicker.value;
+const eraserImg = document.getElementById("eraser-img");
+let gridDivs = document.getElementsByClassName("divs");
+const rainbowIcon = document.querySelector('#rainbow-img');
+const slider = document.querySelector('.slider');
+let sliderLabel = document.querySelector('.slider-label');
+let randomColor = '';
+let rainbowMode = false;
+const borderRadius = "10px";
+const rainbowImg = "rainbow-img";
 
 
-function divsWrapper (gridSize) {
-    // Variable Declarations
-    const container = document.querySelector('.container')
+
+// Functions 
+function createCanvas (gridSize, color) {
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
     // Create 16x16 divs
     for (i = 0; i < (gridSize * gridSize); i++) {
-        let divs = document.createElement('div');
-        divs.classList.add('divs');
-        container.insertAdjacentElement('beforeend', divs);
-        divs.addEventListener('mouseover', function() {
-            divs.style.backgroundColor = 'black';
-        });
-    }};
+        let div = document.createElement('div');
+        div.classList.add("divs");
+        div.addEventListener('mouseover', changeDivColor);
+        container.appendChild(div)
+    }
 
-// Function Call
-divsWrapper(16);
+    // Proportionally Set Corners of Screen Divs To Be Rounded
+    gridDivs[0].style.borderTopLeftRadius = borderRadius;
+    gridDivs[gridSize - 1].style.borderTopRightRadius = borderRadius;
+    gridDivs[(gridSize * gridSize) - 1].style.borderBottomRightRadius = borderRadius;
+    gridDivs[(gridSize * gridSize) - gridSize].style.borderBottomLeftRadius = borderRadius;
+};
 
-// Event Listener: Clear Game on Click
+// Initial Function Call
+createCanvas(16, chosenColor);
+
+// Clear Screen on Click
 clearBtn.addEventListener('click', function () {
-    location.reload();
+    // Set Divs Background To White
+    for(let i=0;i<gridDivs.length;i++) {
+        gridDivs[i].style.backgroundColor = "#FFFFFF"; 
+    };
 });
+
+colorPicker.onchange = (e) => setPenColor(e.target.value);
+eraserImg.onclick = () => setPenColor('#FFFFFF');
+rainbowIcon.onclick = () => setPenColor(rainbowImg);
+slider.onchange = (e) => gridReset(e.target.value);
+slider.onmousemove = (e) => updateSizeValue(e.target.value);
+
+// Reset Grid Size
+function gridReset(value) {
+    // Clear Grid
+    container.innerText = '';
+    // Reset Grid
+    createCanvas(value, chosenColor);
+}
+
+// Update Grid Size HTML Text
+function updateSizeValue(value) {
+    sliderLabel.textContent = `${value} x ${value}`;
+};
+
+function setPenColor(value) {
+    if (value == rainbowImg) {
+        randomColor = Math.floor(Math.random()*16777215).toString(16);
+        chosenColor = `#${randomColor}`;
+        rainbowMode = true;
+        return chosenColor;
+    }
+
+    rainbowMode = false;
+    chosenColor = value;
+    return chosenColor;
+};
+
+function changeDivColor(e) {
+    if (rainbowMode) {
+        e.target.style.backgroundColor = setPenColor(rainbowImg);
+    }
+
+    e.target.style.backgroundColor = chosenColor;
+};
 
 
